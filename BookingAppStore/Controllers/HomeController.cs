41d6b12213c92ed9395bd988c148e7b7bce2274d
David <david.chalyan25@gmail.com>
2022-06-05 21:37:29 +0300

    added styles and designed the home page

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookingAppStore.Models;
 
 namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            var books = db.Books;
+            ViewBag.Books = books;
             return View();
         }
-
+        
+        public ActionResult Buy(int? id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            //добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            //соохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
